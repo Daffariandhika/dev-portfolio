6d6c6f219b4f607477f919045fa71ca0294528aa
Riandhika <muhammaddaffariandhika@gmail.com>
2025-09-15 02:36:52 +0700

    set up dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import preact from "@astrojs/preact";
+import compress from "astro-compress";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [preact(), tailwind(), compress()],
+});
