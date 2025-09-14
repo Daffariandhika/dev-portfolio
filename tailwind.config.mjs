5531705cafe8f769b5733120e0cade84784a644b
Riandhika <muhammaddaffariandhika@gmail.com>
2025-09-15 05:08:55 +0700

    set up screen breakpoints

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			screens: {
+				'xs': '410px',
+				'4xl': '1800px'
+			},
 		},
 	},
 	plugins: [],
